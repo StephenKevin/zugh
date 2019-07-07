e95fd173bac8053061d4d9c30f0c37fdb810cd13
StephenKwen <hyuncankun@outlook.com>
2019-07-08 02:20:19 +0800

    add decorator package

---
+++ decorator.py/query.py
@@ -0,0 +1,14 @@
+
+
+def query_func(query):
+    """decorator to generate query function from query object"""
+
+    statement = str(query)
+
+    def func():
+        """"""
+        statement = statement
+
+        # TODO
+
+    return func
